3a5196ea1cf5e1412f1b02df83861104af022b10
Beqaelbaqidze <83675524+Beqaelbaqidze@users.noreply.github.com>
2023-03-23 14:52:14 +0400

    Update index.js

--- index.js
+++ index.js
@@ -357,18 +357,3 @@
 
 })(window);
 
-
-var i = 0;
-var col = new Array("red","green","aqua", "orange", "blue", "yellow");
-
-function change(){
-    document.getElementById("text").style.color = col[i];
-    document.getElementById("text").style.transition = "4s";
-    i++;
-    if(i>col.length){
-        i=0;
-    }
-    window.setTimeout("change()",3000);
-};
-
-document.getElementById("text").addEventListener("",change());;
